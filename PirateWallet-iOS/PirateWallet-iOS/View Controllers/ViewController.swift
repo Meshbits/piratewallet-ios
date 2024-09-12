fd71c78cccd5be491925b75d4d1ce1c1362b6d58
Lokesh Sehgal <lastw0nd3r@gmail.com>
2024-09-13 00:49:09 +0530

    Pirate Wallet Initial checkin

---
+++ PirateWallet-iOS/PirateWallet-iOS/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PirateWallet-iOS
+//
+//  Created by Lokesh on 13/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
