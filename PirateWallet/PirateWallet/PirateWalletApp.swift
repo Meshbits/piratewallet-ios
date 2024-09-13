f70a91298c6129b6dd6c289da1690f11005d9d28
Lokesh Sehgal <lastw0nd3r@gmail.com>
2024-09-13 10:56:00 +0530

    Replaced storyboard changes with SwiftUI Codebase as our original project was in SwiftUI

---
+++ PirateWallet/PirateWallet/PirateWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  PirateWalletApp.swift
+//  PirateWallet
+//
+//  Created by Lokesh on 13/09/24.
+//
+
+import SwiftUI
+
+@main
+struct PirateWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
